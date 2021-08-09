97e914cdf5350d852086e4c13cfff3b6c6b3ed64
Chia-Jung Hsu <appolloford@gmail.com>
2021-08-09 11:54:57 +0200

    Test dropzone

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
+import {useDropzone} from 'react-dropzone'
 import script from './python/script.py';
-import logo from './logo.svg';
 import './App.css';
+
+function MyDropzone() {
+  const onDrop = useCallback((acceptedFiles) => {
+    acceptedFiles.forEach((file) => {
+      const reader = new FileReader()
+
+      reader.onabort = () => console.log('file reading was aborted')
+      reader.onerror = () => console.log('file reading has failed')
+      reader.onload = () => {
+        // Do whatever you want with the file contents
+        // const binaryStr = reader.result
+        // console.log(binaryStr)
+        window.pyodide.loadPackage(['astropy']).then(() => {
+          const output = window.pyodide.runPython(`
+            # import numpy as np
+            # from astropy.io import fits
+            # fits.open(${reader.readAsArrayBuffer(file)})
+            print(1+2)
+          `)
+          console.log(output)
+        })
+      }
+      reader.readAsArrayBuffer(file)
+    })
+    
+  }, [])
+  const {getRootProps, getInputProps} = useDropzone({onDrop})
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      <p>Drag 'n' drop some files here, or click to select files</p>
+    </div>
+  )
+}
 
 function App() {
   const [output, setOutput] = useState("(loading...)");
+  const [isClicked, setClicked] = useState(false);
 
   const runScript = code => {
     window.pyodide.loadPackage([]).then(() => {
@@ -24,10 +60,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          5 + 7 = {output}
-        </p>
+        <div className="flex-container">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <p>
+            5 + 7 = {output}
+          </p>
+          <button onClick={() => setClicked(!isClicked)}>{''+isClicked}</button>
+        </div>
+        <MyDropzone/>
       </header>
     </div>
   );
