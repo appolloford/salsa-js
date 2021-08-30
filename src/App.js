fb48b9c22dea7bb70f4930a11ef1f2d69125d0fc
Chia-Jung Hsu <appolloford@gmail.com>
2021-08-30 17:53:50 +0200

    Remove some useless codes

--- src/App.js
+++ src/App.js
@@ -7,13 +7,7 @@ import './App.css';
 function App() {
   const [output, setOutput] = useState("(loading...)");
   const [isClicked, setClicked] = useState(false);
-  const dummy = [{
-    x: [1, 2, 3],
-    y: [2, 6, 3],
-    type: 'scatter',
-    mode: 'lines+markers',
-    marker: {color: 'red'},
-  }]
+  const dummy = [{}]
   const [plotdata, setPlotdata] = useState(dummy);
 
   const runScript = code => {
@@ -33,42 +27,37 @@ function App() {
 
   const onDrop = useCallback((acceptedFiles) => {
     acceptedFiles.forEach((file) => {
-      return new Promise((resolve, reject) => {
-        const reader = new FileReader()
+      const reader = new FileReader()
 
-        reader.onabort = () => console.log('file reading was aborted')
-        reader.onerror = () => console.log('file reading has failed')
-        reader.onload = () => {
-          // Do whatever you want with the file contents
-          // const binaryStr = reader.result
-          // console.log(binaryStr)
-          window.jsarray = new Float32Array(reader.result);
-          window.pyodide.loadPackage(['astropy']).then(() => {
-            const ret = window.pyodide.runPython(`
-                from io import BytesIO
-                from js import jsarray
-                from astropy.io import fits
-                array = jsarray.to_py().tobytes()
-                print(type(array))
-                print(array)
-                raw = fits.open(BytesIO(array), mode="readonly")
-                data = raw[0].data
-                data.tolist()
-              `)
-            console.log("return value", ret.toJs())
-            console.log("return value", ret.toJs()[0])
-            console.log("return value", ret.toJs()[0][0])
-            resolve(ret.toJs())
-            setPlotdata([{
-              x: ret.toJs()[0][0].keys(),
-              y: ret.toJs()[0][0],
-              type: 'scatter',
-              mode: 'lines+markers',
-            }])
-          })
-        }
-        reader.readAsArrayBuffer(file)
-      })
+      reader.onabort = () => console.log('file reading was aborted')
+      reader.onerror = () => console.log('file reading has failed')
+      reader.onload = () => {
+        // Do whatever you want with the file contents
+        window.jsarray = new Float32Array(reader.result);
+        window.pyodide.loadPackage(['astropy']).then(() => {
+          const ret = window.pyodide.runPython(`
+              from io import BytesIO
+              from js import jsarray
+              from astropy.io import fits
+              array = jsarray.to_py().tobytes()
+              print(type(array))
+              print(array)
+              raw = fits.open(BytesIO(array), mode="readonly")
+              data = raw[0].data
+              data.tolist()
+            `)
+          console.log("return value", ret.toJs())
+          console.log("return value", ret.toJs()[0])
+          console.log("return value", ret.toJs()[0][0])
+          setPlotdata([{
+            x: ret.toJs()[0][0].keys(),
+            y: ret.toJs()[0][0],
+            type: 'scatter',
+            mode: 'lines+markers',
+          }])
+        })
+      }
+      reader.readAsArrayBuffer(file)
     })
     
   }, [])
@@ -78,7 +67,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="flex-container">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <p>
             5 + 7 = {output}
           </p>
@@ -89,19 +77,6 @@ function App() {
           <p>Drag 'n' drop some files here, or click to select files</p>
         </div>
         <div>
-          {/* <Plot
-            data={[
-              {
-                x: [1, 2, 3],
-                y: [2, 6, 3],
-                type: 'scatter',
-                mode: 'lines+markers',
-                // marker: {color: 'red'},
-              },
-              // {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
-            ]}
-            // layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
-          /> */}
           <Plot data={plotdata}/>
         </div>
       </header>
