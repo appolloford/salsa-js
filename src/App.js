464579b5f85540788c9770a41be41939157eee94
Chia-Jung Hsu <appolloford@gmail.com>
2021-08-30 17:46:39 +0200

    Plot the uploaded fits data

--- src/App.js
+++ src/App.js
@@ -4,50 +4,17 @@ import Plot from 'react-plotly.js';
 import script from './python/script.py';
 import './App.css';
 
-function MyDropzone() {
-  const onDrop = useCallback((acceptedFiles) => {
-    acceptedFiles.forEach((file) => {
-      const reader = new FileReader()
-
-      reader.onabort = () => console.log('file reading was aborted')
-      reader.onerror = () => console.log('file reading has failed')
-      reader.onload = () => {
-        // Do whatever you want with the file contents
-        const binaryStr = reader.result
-        console.log(binaryStr)
-        window.jsarray = new Float32Array(reader.result);
-        window.pyodide.loadPackage(['astropy']).then(() => {
-          const ret = window.pyodide.runPython(`
-            from io import BytesIO
-            from js import jsarray
-            from astropy.io import fits
-            array = jsarray.to_py().tobytes()
-            print(type(array))
-            print(array)
-            raw = fits.open(BytesIO(array), mode="readonly")
-            data = raw[0].data
-            data.tolist()
-          `)
-          console.log("return value", ret.toJs())
-        })
-      }
-      reader.readAsArrayBuffer(file)
-    })
-    
-  }, [])
-  const {getRootProps, getInputProps} = useDropzone({onDrop})
-
-  return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      <p>Drag 'n' drop some files here, or click to select files</p>
-    </div>
-  )
-}
-
 function App() {
   const [output, setOutput] = useState("(loading...)");
   const [isClicked, setClicked] = useState(false);
+  const dummy = [{
+    x: [1, 2, 3],
+    y: [2, 6, 3],
+    type: 'scatter',
+    mode: 'lines+markers',
+    marker: {color: 'red'},
+  }]
+  const [plotdata, setPlotdata] = useState(dummy);
 
   const runScript = code => {
     window.pyodide.loadPackage([]).then(() => {
@@ -64,6 +31,49 @@ function App() {
     })
   })
 
+  const onDrop = useCallback((acceptedFiles) => {
+    acceptedFiles.forEach((file) => {
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader()
+
+        reader.onabort = () => console.log('file reading was aborted')
+        reader.onerror = () => console.log('file reading has failed')
+        reader.onload = () => {
+          // Do whatever you want with the file contents
+          // const binaryStr = reader.result
+          // console.log(binaryStr)
+          window.jsarray = new Float32Array(reader.result);
+          window.pyodide.loadPackage(['astropy']).then(() => {
+            const ret = window.pyodide.runPython(`
+                from io import BytesIO
+                from js import jsarray
+                from astropy.io import fits
+                array = jsarray.to_py().tobytes()
+                print(type(array))
+                print(array)
+                raw = fits.open(BytesIO(array), mode="readonly")
+                data = raw[0].data
+                data.tolist()
+              `)
+            console.log("return value", ret.toJs())
+            console.log("return value", ret.toJs()[0])
+            console.log("return value", ret.toJs()[0][0])
+            resolve(ret.toJs())
+            setPlotdata([{
+              x: ret.toJs()[0][0].keys(),
+              y: ret.toJs()[0][0],
+              type: 'scatter',
+              mode: 'lines+markers',
+            }])
+          })
+        }
+        reader.readAsArrayBuffer(file)
+      })
+    })
+    
+  }, [])
+  const {getRootProps, getInputProps} = useDropzone({onDrop})
+
   return (
     <div className="App">
       <header className="App-header">
@@ -74,21 +84,25 @@ function App() {
           </p>
           <button onClick={() => setClicked(!isClicked)}>{''+isClicked}</button>
         </div>
-        <MyDropzone />
+        <div {...getRootProps()}>
+          <input {...getInputProps()} />
+          <p>Drag 'n' drop some files here, or click to select files</p>
+        </div>
         <div>
-          <Plot
+          {/* <Plot
             data={[
               {
                 x: [1, 2, 3],
                 y: [2, 6, 3],
                 type: 'scatter',
                 mode: 'lines+markers',
-                marker: {color: 'red'},
+                // marker: {color: 'red'},
               },
-              {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+              // {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
             ]}
-            layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
-          />
+            // layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
+          /> */}
+          <Plot data={plotdata}/>
         </div>
       </header>
     </div>
