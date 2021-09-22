51a66e41496af89e631b390725889a8570bf51b5
Chia-Jung Hsu <appolloford@gmail.com>
2021-09-22 12:49:05 +0200

    Set x values and get header

--- src/App.js
+++ src/App.js
@@ -5,7 +5,41 @@ import './App.css';
 
 function App() {
 
-  const [plotdata, setPlotdata] = useState([{}]);
+  const initlayout = {
+    title: {
+      text:'Plot Title',
+      font: {
+        family: 'Courier New, monospace',
+        size: 24
+      },
+      xref: 'paper',
+      x: 0.05,
+    },
+    xaxis: {
+      title: {
+        text: 'x Axis',
+        font: {
+          family: 'Courier New, monospace',
+          size: 18,
+          color: '#7f7f7f'
+        }
+      },
+      tickformat: '.3e'
+    },
+    yaxis: {
+      title: {
+        text: 'y Axis',
+        font: {
+          family: 'Courier New, monospace',
+          size: 18,
+          color: '#7f7f7f'
+        }
+      },
+    }
+  };
+
+  const [plotdata, setPlotData] = useState([{}]);
+  const [plotlayout, setPlotLayout] = useState(initlayout);
 
   const onDrop = useCallback((acceptedFiles) => {
     acceptedFiles.forEach((file) => {
@@ -17,7 +51,7 @@ function App() {
         // Do whatever you want with the file contents
         window.jsarray = new Float32Array(reader.result);
         window.pyodide.loadPackage(['astropy']).then(() => {
-          const rawdata = window.pyodide.runPython(`
+          const ydata = window.pyodide.runPython(`
               from io import BytesIO
               from js import jsarray
               from astropy.io import fits
@@ -28,12 +62,41 @@ function App() {
               data = raw[0].data
               data.tolist()
             `)
-          // console.log("return value", rawdata.toJs())
-          // console.log("return value", rawdata.toJs()[0])
-          // console.log("return value", rawdata.toJs()[0][0])
-          setPlotdata([{
-            x: rawdata.toJs()[0][0].keys(),
-            y: rawdata.toJs()[0][0],
+          // console.log("return value", ydata.toJs())
+          // console.log("return value", ydata.toJs()[0])
+          // console.log("return value", ydata.toJs()[0][0])
+          const xdata = window.pyodide.runPython(`
+              from io import BytesIO
+              from js import jsarray
+              from astropy.io import fits
+              array = jsarray.to_py().tobytes()
+              raw = fits.open(BytesIO(array), mode="readonly")
+              header = raw[0].header
+              naxis = header["NAXIS1"]
+              reval = header["CRVAL1"]
+              repix = header["CRPIX1"]
+              delta = header["CDELT1"]
+              xdata = [reval + (i-repix) * delta for i in range(naxis)]
+              xdata
+            `)
+          // console.log("xdata", xdata.toJs())
+          const header = window.pyodide.runPython(`
+            from io import BytesIO
+            from js import jsarray
+            from astropy.io import fits
+            array = jsarray.to_py().tobytes()
+            raw = fits.open(BytesIO(array), mode="readonly")
+            header = raw[0].header
+            print(header.items())
+            # header.items()
+            dict(header)
+          `)
+          console.log("header", header.toJs())
+          console.log("CRPIX1", header.get("CRPIX1"))
+
+          setPlotData([{
+            x: xdata.toJs(),
+            y: ydata.toJs()[0][0],
             type: 'scatter',
             mode: 'lines+markers',
           }])
@@ -53,7 +116,7 @@ function App() {
           <p>Drag 'n' drop some files here, or click to select files</p>
         </div>
         <div>
-          <Plot data={plotdata}/>
+          <Plot data={plotdata} layout={plotlayout}/>
         </div>
       </header>
     </div>
